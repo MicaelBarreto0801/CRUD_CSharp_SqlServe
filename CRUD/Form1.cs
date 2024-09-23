82c4191c4c974fe66dc96cd2cf2827654fdda7f0
Micael Barreto <micaeldistake55@gmail.com>
2024-09-23 18:03:10 -0300

    Feito conexão com Banco de dados e inserido o primeiro dado na Tabela

--- CRUD/Form1.cs
+++ CRUD/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CRUD
 {
@@ -17,9 +18,40 @@ namespace CRUD
             InitializeComponent();
         }
 
+        SqlConnection sqlCon = null;
+        private string strCon = @"Integrated Security=SSPI;Persist Security Info=False;Data Source=DESKTOP-OJ8JR8I";
+        private string strSql = string.Empty;
+
+
+
         private void tsbSalvar_Click(object sender, EventArgs e)
         {
+            strSql = "insert into Funcionarios (Id, Nome, Endereco, CEP, Bairro, Cidade, UF, Telefone) values (@Id, @Nome, @Endereco, @CEP, @Bairro, @Cidade, @UF, @Telefone)";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
 
+            comando.Parameters.Add("@Id", SqlDbType.Int).Value = txtId.Text;
+            comando.Parameters.Add("@Nome", SqlDbType.VarChar).Value = txtNome.Text;
+            comando.Parameters.Add("@Endereco", SqlDbType.VarChar).Value = txtEndereco.Text;
+            comando.Parameters.Add("@CEP", SqlDbType.VarChar).Value = mskCEP.Text;
+            comando.Parameters.Add("@Bairro", SqlDbType.VarChar).Value = txtBairro.Text;
+            comando.Parameters.Add("@Cidade", SqlDbType.VarChar).Value = txtCidade.Text;
+            comando.Parameters.Add("@UF", SqlDbType.VarChar).Value = txtUF.Text;
+            comando.Parameters.Add("@Telefone", SqlDbType.VarChar).Value = mskTelefone.Text;
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro realizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
